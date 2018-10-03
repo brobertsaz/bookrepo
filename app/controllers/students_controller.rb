c04dad22064fb849211832d127ca1833f92bc1de
Bob Roberts <cardsfanbob@gmail.com>
2018-10-03 14:27:30 -0500

    added the edit and delte for the teachers and students

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,51 @@
 class StudentsController < ApplicationController
   before_action :authenticate_teacher!
+  before_action :verify_admin
+  before_action :set_student, except: :new
 
   def new
     @student = Student.new
   end
+
+  def edit
+  end
+
+  def update
+    @student.update student_params
+    if @student.save
+      flash[:notice] = 'Student successfully updated.'
+      redirect_to dashboard_root_path
+    else
+      flash[:error] = 'Student unable to be updated.'
+      render 'edit'
+    end
+  end
+
+  def destroy
+    if @student.destroy
+      flash[:notice] = 'Student successfully deleted.'
+    else
+      flash[:error] = 'Student unable to be deleted.'
+    end
+    redirect_to dashboard_root_path
+  end
+
+  private
+
+  def set_student
+    @student = Student.find params[:id]
+  end
+
+  def verify_admin
+    unless current_teacher&.admin?
+      flash[:error] = 'You are not an admin.'
+      redirect_to dashboard_root_path
+    end
+  end
+
+  def student_params
+    params.require(:student).permit(:email, :last_name, :first_name, :school_id)
+  end
+
 
 end
--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -1,7 +1,49 @@
 class TeachersController < ApplicationController
   before_action :authenticate_teacher!
+  before_action :verify_admin
+  before_action :set_teacher, except: :new
 
   def new
-    @Teacher = Teacher.new
+    @teacher = Teacher.new
+  end
+
+  def edit
+  end
+
+  def update
+    @teacher.update teacher_params
+    if @teacher.save
+      flash[:notice] = 'Teacher successfully updated.'
+      redirect_to dashboard_root_path
+    else
+      flash[:error] = 'Teacher unable to be updated.'
+      render 'edit'
+    end
+  end
+
+  def destroy
+    if @teacher.destroy
+      flash[:notice] = 'Teacher successfully deleted.'
+    else
+      flash[:error] = 'Teacher unable to be deleted.'
+    end
+    redirect_to dashboard_root_path
+  end
+
+  private
+
+  def set_teacher
+    @teacher = Teacher.find params[:id]
+  end
+
+  def verify_admin
+    unless current_teacher&.admin?
+      flash[:error] = 'You are not an admin.'
+      redirect_to dashboard_root_path
+    end
+  end
+
+  def teacher_params
+    params.require(:teacher).permit(:email, :last_name, :first_name, :school_id)
   end
 end
